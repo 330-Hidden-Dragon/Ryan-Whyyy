8eac6d0a1aad572bbfe7b06db24df4c4670886fe
skorlir <skorlir@gmail.com>
2016-02-28 13:43:33 -0600

    Very basic routing and create issue page controller

---
+++ src/js/create-issue/index.js
@@ -0,0 +1,69 @@
+
+var RecordRTC = require('recordrtc')
+  , q1        = require('../qq.js').q1
+
+module.exports = function () {
+  var mediaRequests = { audio: true, video: true }
+    , recordBtn     = q1('.video-upload-btn')
+    , video         = q1('video')
+    , placeholder   = q1('p.placeholder')
+    , description   = q1('textarea')
+    , recorder
+    , listenerId
+
+  description.addEventListener('focus', function () {
+    if (description.value === description.textContent)
+      description.value  = ''
+  })
+
+  description.addEventListener('blur', function () {
+    if (description.value.length === 0) {
+      description.value = description.textContent
+    }
+  })
+
+  function stopRecording () {
+    recorder.stopRecording(function (videoUrlObject) {
+      video.src = videoUrlObject
+      video.controls = true
+
+      // NOTE(jordan): save this to the issue model
+      var blob = recorder.getBlob()
+    })
+  }
+
+  function startRecording (stream) {
+    // NOTE(jordan): hide placeholder
+    placeholder.style.display = 'none'
+
+    // NOTE(jordan): show stream while recording
+    video.src = URL.createObjectURL(stream)
+
+    // NOTE(jordan): set up RecordRTC
+    recorder = RecordRTC(stream, {
+      mimeType: 'video/webm',
+      bitsPerSecond: 128000
+    })
+
+    // NOTE(jordan): start RecordRTC
+    recorder.startRecording()
+    // NOTE(jordan): change its click handler to stop the recording
+    recordBtn.removeEventListener('click', listenerId)
+    listenerId = recordBtn.addEventListener('click', stopRecording)
+  }
+
+  function error (error) {
+    alert('Could not get access to webcam')
+    console.error(error)
+  }
+
+  listenerId = recordBtn.addEventListener('click', function (evt) {
+    console.log(arguments)
+    console.log(evt)
+    evt.preventDefault()
+    ;(navigator.mediaDevices.getUserMedia
+      || navigator.getUserMedia).call(navigator, mediaRequests,
+        startRecording,
+        error)
+  })
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
 // NOTE(jordan): this will be the 'entry' point for our application
+var createIssuePageController = require('./create-issue')
 
-console.log('Hello!')
+var currentPage = window.location.pathname
+  .split('/')   // this
+  .reverse()[0] // is
+  .replace(/\.html/g, '') // inefficient
 
+console.log('At page', currentPage)
+
+switch(currentPage) {
+  case 'create-issue':
+    createIssuePageController()
+    break
+  default:
+    break
+}
