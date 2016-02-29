6d0be93b0cab20db247823844f3233618b8148b3
skorlir <skorlir@gmail.com>
2016-02-29 14:24:44 -0600

    Turn long press / right click into click

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,15 @@
 var router = require('./router')
   , setupNav = require('./nav')
 
+// NOTE(jordan): make a long-press turn into a click
+document.addEventListener('contextmenu', function (event) {
+  event.preventDefault()
+  event.stopPropagation()
+  console.log(event.target)
+  event.target.click()
+  return false
+})
+
 setupNav()
 
 router.load()
